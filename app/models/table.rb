0bdd26b512f5325fdfe78bd9f55bad1098adfa7b
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-09-04 12:40:18 -0500

    Create TODOs and Update Gemfile - Added rake-notes gem to easily locate my TODOs - Removed dotenv gem

--- app/models/table.rb
+++ app/models/table.rb
@@ -1,6 +1,7 @@
 class Table < ApplicationRecord
   has_many :reservations
 
+  #TODO
   #When a table is booked and a user tries to book the same table at the same time this error message should display:
   # "Sorry, that table has already been reserved. Please pick another time."
 
