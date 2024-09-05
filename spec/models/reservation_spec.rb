d97740d6025c6bb93d6a0ca037e047f10f88cbc9
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-09-05 12:23:01 -0500

    Test Table Model - Test Table Model at_capacity?, can_accommodate?, and available? methods

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,19 +1,20 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Reservation, type: :model do
   # TODO belongs_to table keeps failing
   it { should belong_to(:table) }
 
   context "when checking for availability" do
-    let (:table) {Table.create(name: 'Table 1', seats:4)}
+    let (:table) { Table.create(name: "Table 1", seats: 4) }
     let (:reservation) do
-    Reservation.new(
-      name:"Alana Edwards",
-      description: "Table for two",
-      table: table,
-      start_time: Time.now,
-      end_time: 1.hour.from_now,
-      party_size: 2)
+      Reservation.new(
+        name: "Alana Edwards",
+        description: "Table for two",
+        table: table,
+        start_time: Time.now,
+        end_time: 1.hour.from_now,
+        party_size: 2,
+      )
     end
 
     it "shows an error when the table is not available" do
@@ -29,5 +30,4 @@ RSpec.describe Reservation, type: :model do
       expect(reservation.errors[:base]).to be_empty
     end
   end
-
 end
